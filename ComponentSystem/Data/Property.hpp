486f8e987dcb75b913138cb1d5fba70203d39259
JeppeNielsen <nielsen_jeppe@hotmail.com>
2016-01-16 22:37:13 +0100

    reorganized files, added Event and property classes

---
+++ ComponentSystem/Data/Property.hpp
@@ -0,0 +1,69 @@
+//
+//  Property.h
+//  EntitySystem
+//
+//  Created by Jeppe Nielsen on 08/12/15.
+//  Copyright (c) 2015 Jeppe Nielsen. All rights reserved.
+//
+
+#pragma once
+#include "Event.hpp"
+
+template<class Value>
+class Property {
+private:
+    Value value;
+    
+    void Set(const Value& newValue) {
+        if (value==newValue) return;
+        value = newValue;
+        Changed(value);
+    }
+public:
+    Event<Value> Changed;
+
+    const Value& operator() () const { return value; }
+    void operator = (const Value& v) { Set(v); }
+    void operator = (const Property<Value>& v) { Set(v.value); }
+    Value operator - () const { return -value; }
+    
+    Value operator + (const Value& v) const { return value + v; }
+    Value operator - (const Value& v) const { return value - v; }
+    Value operator * (const Value& v) const { return value * v; }
+    Value operator / (const Value& v) const { return value / v; }
+    Value operator ++ (int) { Value temp = value; Set(value+1); return temp; }
+    Value operator -- (int) { Value temp = value; Set(value-1); return temp; }
+    Value operator ++ () { Set(value+1); return value; }
+    Value operator -- () { Set(value-1); return value; }
+    
+    Value operator + (const Property<Value>& v) const { return value + v.value; }
+    Value operator - (const Property<Value>& v) const { return value - v.value; }
+    Value operator * (const Property<Value>& v) const { return value * v.value; }
+    Value operator / (const Property<Value>& v) const { return value / v.value; }
+    
+    const Value& operator += (const Value& v) { Set(value + v); return value; }
+    const Value& operator -= (const Value& v) { Set(value - v); return value; }
+    const Value& operator *= (const Value& v) { Set(value * v); return value; }
+    const Value& operator /= (const Value& v) { Set(value / v); return value; }
+
+    const Value& operator += (const Property<Value&> v) { Set(value + v.value); return value; }
+    const Value& operator -= (const Property<Value>& v) { Set(value - v.value); return value; }
+    const Value& operator *= (const Property<Value>& v) { Set(value * v.value); return value; }
+    const Value& operator /= (const Property<Value>& v) { Set(value / v.value); return value; }
+
+    friend Value operator+(const Value& value, Property<Value>& property) {
+        return value + property.value;
+    }
+    
+    friend Value operator-(const Value& value, Property<Value>& property) {
+        return value - property.value;
+    }
+    
+    friend Value operator*(const Value& value, Property<Value>& property) {
+        return value * property.value;
+    }
+    
+    friend Value operator/(const Value& value, Property<Value>& property) {
+        return value / property.value;
+    }
+};
